96a7b322ec53483d177595ce344420ee25b2245a
JavierHR <javi3rhr@gmail.com>
2022-12-01 18:06:38 +0100

    Change Card bg style

--- src/pages/BackOffice/AdminHome/components/HomeCard.tsx
+++ src/pages/BackOffice/AdminHome/components/HomeCard.tsx
@@ -5,7 +5,7 @@ interface HomeCardProps {
 
 export const HomeCard = ({ title, value }: HomeCardProps) => {
 	return (
-		<div className='w-full rounded-lg bg-white px-4 py-5 shadow'>
+		<div className='w-full rounded-lg bg-slate-100 px-4 py-5 shadow'>
 			<div className='truncate text-sm font-medium text-gray-500'>{title}</div>
 			<div className='mt-1 text-3xl font-semibold text-gray-900'>{value}</div>
 		</div>
