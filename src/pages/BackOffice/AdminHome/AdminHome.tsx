64c658273a1221b9a79736af710e2b3d75c1fe6d
JavierHR <javi3rhr@gmail.com>
2022-12-03 18:03:35 +0100

    Update AdminHome cards

--- src/pages/BackOffice/AdminHome/AdminHome.tsx
+++ src/pages/BackOffice/AdminHome/AdminHome.tsx
@@ -1,18 +1,46 @@
-import { getUsers } from '@/services';
+import { getCourts, getReservations, getSports, getUsers } from '@/services';
 import { useEffect, useState } from 'react';
 import { HomeCard } from './components';
 
 const AdminHome = () => {
 	const [users, setUsers] = useState(0);
+	const [courts, setCourts] = useState(0);
+	const [sports, setSports] = useState(0);
+	const [reservations, setReservations] = useState(0);
 
 	useEffect(() => {
-		getUsers()
-			.then(response => {
-				setUsers(response.data.length);
-			})
-			.catch(error => {
-				console.error(error);
-			});
+		try {
+			getUsers()
+				.then(response => {
+					setUsers(response.data.length);
+				})
+				.catch(error => {
+					throw error;
+				});
+			getCourts()
+				.then(response => {
+					setCourts(response.data.length);
+				})
+				.catch(error => {
+					throw error;
+				});
+			getSports()
+				.then(response => {
+					setSports(response.data.length);
+				})
+				.catch(error => {
+					throw error;
+				});
+			getReservations()
+				.then(response => {
+					setReservations(response.data.length);
+				})
+				.catch(error => {
+					throw error;
+				});
+		} catch (error) {
+			console.log(error);
+		}
 	}, []);
 	return (
 		<div className='h-screen flex-1 p-7 text-gray-100'>
@@ -20,9 +48,9 @@ const AdminHome = () => {
 			<div className='container mx-auto mt-12'>
 				<div className='mb-6 grid grid-cols-1 gap-7 sm:grid-cols-2 lg:grid-cols-3'>
 					<HomeCard title='Usuarios' value={String(users)} />
-					<HomeCard title='Pistas disponibles' value={'7'} />
-					<HomeCard title='Reservas totales' value={'400'} />
-					<HomeCard title='Reservas de hoy' value={'13'} />
+					<HomeCard title='Deportes operativos' value={String(sports)} />
+					<HomeCard title='Pistas disponibles' value={String(courts)} />
+					<HomeCard title='Reservas totales' value={String(reservations)} />
 				</div>
 			</div>
 		</div>
